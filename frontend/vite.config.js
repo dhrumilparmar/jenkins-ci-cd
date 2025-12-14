ac0ca7db81d3a9a1254af80a11c2a9e14ebf9d0c
dhrumilparmar <dhrumilparmar27481@gmail.com>
2025-12-14 14:56:52 +0530

    updated the docker compose file

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,8 +6,8 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api/": "http://localhost:5000",
-      "/uploads/": "http://localhost:5000",
+      "/api/": "http://backend:5000",
+      "/uploads/": "http://backend:5000",
     },
   },
 });
